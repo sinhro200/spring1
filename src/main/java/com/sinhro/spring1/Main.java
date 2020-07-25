1798e4029402c3154cb5d15348507f08c2c9d62a
sinhro200 <sinhro-200@mail.ru>
2020-07-25 12:56:14 +0300

    second attempt

--- src/main/java/com/sinhro/spring1/Main.java
+++ src/main/java/com/sinhro/spring1/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring1Application {
+public class Main {//Spring1Application
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring1Application.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
